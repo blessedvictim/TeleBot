2da5022649b5d2cd435527f626f4bf1d6bbb1490
theonlylies <tequila161rus@gmail.com>
2016-11-27 20:01:29 +0300

    78b

--- TeleBot.py
+++ TeleBot.py
@@ -28,8 +28,8 @@ WEBHOOK_HOST = '172.31.22.9'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
-WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Путь к приватному ключу
+WEBHOOK_SSL_CERT = 'cert/webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = 'cert/webhook_pkey.pem'  # Путь к приватному ключу
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % ('265431846:AAGobX591NC5o4PTeqMj8wei1YOprQKyDlU')
