179ba80a80f2f08175c697ad9d5036a9ec97782a
theonlylies <tequila161rus@gmail.com>
2016-11-27 14:18:25 +0300

    1

--- TeleBot.py
+++ TeleBot.py
@@ -24,12 +24,12 @@ class WebhookServer(object):
         else:
             raise cherrypy.HTTPError(403)
 
-WEBHOOK_HOST = '52.90.242.186'
+WEBHOOK_HOST = '172.31.22.9'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
-WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Путь к приватному ключу
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % ('265431846:AAGobX591NC5o4PTeqMj8wei1YOprQKyDlU')
