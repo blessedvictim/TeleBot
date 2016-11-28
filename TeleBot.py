caca7bd9665bec5998977e13ea62fd8bdae4a5ac
theonlylies <tequila161rus@gmail.com>
2016-11-28 03:53:57 +0300

    q

--- TeleBot.py
+++ TeleBot.py
@@ -6,71 +6,46 @@ import requests
 from PIL import Image
 from io import BytesIO
 from bs4 import BeautifulSoup as bs
-import cherrypy
-
-# Наш вебхук-сервер
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            # Эта функция обеспечивает проверку входящего сообщения
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
-
-WEBHOOK_HOST = '172.31.22.9'
-WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-WEBHOOK_SSL_CERT = 'cert/webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = 'cert/webhook_pkey.pem'  # Путь к приватному ключу
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % ('265431846:AAGobX591NC5o4PTeqMj8wei1YOprQKyDlU')
 
 bot = telebot.TeleBot('265431846:AAGobX591NC5o4PTeqMj8wei1YOprQKyDlU')
 
-bot.remove_webhook()
 
- # Ставим заново вебхук
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
-# Указываем настройки сервера CherryPy
-cherrypy.config.update({
-    'server.socket_host': WEBHOOK_LISTEN,
-    'server.socket_port': WEBHOOK_PORT,
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': WEBHOOK_SSL_PRIV
-})
-
- # Собственно, запуск!
-cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
-
-'''
 @bot.message_handler(commands=['start'])
 def SendInfo(message):
-    bot.send_message(message.chat.id, 'Привет я БОООООТ')
+    bot.send_message(message.chat.id, 'Привет я БОТ')
 
 
 @bot.message_handler(commands=['text'])
 def SendInfo(message):
-    images = SearchGoogleImages(message.text, message.chat.id)
+    args = message.text.split(' ')
+    if len(args)== 4 :
+        if len(args[2].split('*'))!=2 :
+            spl = args[2].split('*')
+            width = spl[0]
+            height = spl[1]
+            images = SearchGoogleImages(args[1], message.chat.id, width=width, height=height)
+        else:
+            spl = args[2].split('*')
+            width = spl[0]
+            height = spl[1]
+            images = SearchGoogleImages(args[1], message.chat.id, width=width, height=height, cnt=args[3])
+    elif len(args)== 3 :
+        if len(args[2].split('*'))!=2 :
+            images = SearchGoogleImages(query=args[1], id=message.chat.id,cnt=args[2])
+        else:
+            spl = args[2].split('*')
+            width = spl[0]
+            height = spl[1]
+            images = SearchGoogleImages(args[1], message.chat.id, width=width, height=height)
+    else:
+        images = SearchGoogleImages(query=args[1], id=message.chat.id)
+
     for image in images:
-        bot.send_photo(message.chat.id, open(image, 'rb'))'''
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+        bot.send_photo(message.chat.id, open(image, 'rb'))
 
 
-def SearchGoogleImages(query, id):
+def SearchGoogleImages(query, id ,width=320,height=320,cnt=5):
     path = os.path.abspath(os.curdir)
     path = os.path.join(path, str(id))
     print('path=' + path)
@@ -78,10 +53,10 @@ def SearchGoogleImages(query, id):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    url = 'https://www.google.ru/search?q=' + query[6:] + '&newwindow=1&tbm=isch'
+    url = 'https://www.google.ru/search?q=' + query + '&newwindow=1&tbm=isch'
     request = requests.get(url, headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, Chrome/43.0.2357.134 Safari/537.36'})
-    print(query[6:])
+        'User-Agent': 'Mozilla/50.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, Chrome/43.0.2357.134 Safari/537.36'})
+    print('query='+query)
     print(url)
     soup = bs(request.content, "html.parser")
     # images = soup.find_all('img')
@@ -94,7 +69,7 @@ def SearchGoogleImages(query, id):
 
     imagesPaths = []
 
-    for count, image in enumerate(images[0:1]):
+    for count, image in enumerate(images[0:int(cnt)]):
         '''print(image.get('src')+'\n')
         img = requests.get(image.get('src'))
         image = Image.open(BytesIO(img.content))
@@ -102,12 +77,16 @@ def SearchGoogleImages(query, id):
         image.save(imgpath)
         print(imgpath)
         imagesPaths.append(imgpath)'''
+
         print(str(image) + '\n')
         atr = str(image)[str(image).find('ou":"') + 5:]
         atr = atr[:atr.find('"')]
         img = requests.get(atr)
-        image = Image.open(BytesIO(img.content))
-        imgpath = os.path.join(path, str(count) + '.' + image.format)
+        if width==320 and height==320 :
+            image = Image.open(BytesIO(img.content))
+        else:
+            image = Image.open(BytesIO(img.content)).resize((int(width),int(height)))
+        imgpath = os.path.join(path, str(count) + '.jpg')
         image.save(imgpath)
         print(imgpath)
         imagesPaths.append(imgpath)
@@ -115,13 +94,13 @@ def SearchGoogleImages(query, id):
     return imagesPaths
 
 
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     logging.basicConfig(filename='botlog.log',format='%(filename)s[LINE:%(lineno)d]# %(levelname) -8s [%(asctime)s] %(message)s',level=logging.DEBUG)
-    logging.info('Bot started!')'''
-'''
+    logging.info('Bot started!')
+
 try:
     bot.polling(none_stop=True)
 except Exception:
     logging.critical('ERROR...')
 finally:
-    bot.polling(none_stop=True)'''
+    bot.polling(none_stop=True)
